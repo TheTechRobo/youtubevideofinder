cf049fa5dc0f87e1fc8375306a84a9a4cd2b89ce
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-08-04 15:48:06 -0400

    Add regex verification to the backend

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import *
 from findmedia import *
 
@@ -5,6 +7,8 @@ app = Flask(__name__)
 
 @app.route("/find/<id>")
 async def find(id):
+    if not re.match(r"^[A-Za-z0-9_-]{10}[AEIMQUYcgkosw048]$", id):
+        return {"status": "bad.id", "true": True, "id": None}, 400
     return {
             "status": "find.id",
             "true": True,
@@ -20,6 +24,11 @@ async def find(id):
 @app.route("/")
 async def ui():
     return render_template("init.html")
+
+@app.route("/nojs")
+async def formsubmit():
+    vid = request.args.get("vid") or abort(400)
+    return render_template("fid.html", data=(await find(vid)), vid=vid, nonoscript=True)
 
 @app.route("/ui/fid")
 async def fid():
