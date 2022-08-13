c511be34009278a3f3782e96a4bbab7d5add17a2
nateify <nateify@users.noreply.github.com>
2022-08-13 15:46:43 -0400

    formatting cleanup, add requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import re
 
 from flask import *
+
 from findmedia import *
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ except AttributeError:
     config.ccen = False
     config.ccod = "a"
 
-#@app.route(f"/cc/{config.ccod}")
+# @app.route(f"/cc/{config.ccod}")
 async def cc():
     global GCACHE, FILCACHE, YACACHE, WBMCACHE, IACACHE
     GCACHE = {}
@@ -27,29 +28,52 @@ async def find(id):
     if not re.match(r"^[A-Za-z0-9_-]{10}[AEIMQUYcgkosw048]$", id):
         return {"status": "bad.id", "true": True, "id": None}, 400
     return {
-            "status": "find.id",
-            "true": True,
-            "id": id,
-            "ya": yairc(id),
-            "wbm": wbm(id),
-            "ia": iai(id),
-            "filmot": filmot(id),
-            "ghostarchive": ghostget(id),
-            "keys": ["wbm", "ia", "ghostarchive", "ya", "filmot"]
+        "status": "find.id",
+        "true": True,
+        "id": id,
+        "ya": yairc(id),
+        "wbm": wbm(id),
+        "ia": iai(id),
+        "filmot": filmot(id),
+        "ghostarchive": ghostget(id),
+        "keys": ["wbm", "ia", "ghostarchive", "ya", "filmot"],
     }
+
 
 @app.route("/")
 async def ui():
     return render_template("init.html")
 
+
 @app.route("/api")
 async def api():
-    return render_template("api.html", fields={"id": "The interpreted video ID.", "status": "bad.id if invalid ID.", "keys": "Array of all the service keys."}, services={"archived": "Whether the video is archived or not.", "available": "A link to the archived material if it can be produced; False otherwise.", "capcount": "The number of captures. Currently deprecated - the capture count sent may or may not be the true number of captures. However, it will always be a positive non-zero number if the video is archived.", "lastupdated": "The timestamp the data was retrieved from the server. Used internally to expire cache entries.", "name": "The name of the service. Used in the UI.", "note": "A footnote about the service. This could be different depending on conditions. For example, the Internet Archive has an extra passage if the item is dark.", "rawraw": "The data used to check whether the video is archived on that particular service. For example, for GhostArchive, it would be the HTTP status code.", "suppl": "Supplemental error message. Not used and currently inconsistent with what it returns and when.", "metaonly": "True if only the metadata is archived. This value should not be relied on!", "comments": "True if the comments are archived. This value should not be relied on!"})
+    return render_template(
+        "api.html",
+        fields={
+            "id": "The interpreted video ID.",
+            "status": "bad.id if invalid ID.",
+            "keys": "Array of all the service keys.",
+        },
+        services={
+            "archived": "Whether the video is archived or not.",
+            "available": "A link to the archived material if it can be produced; False otherwise.",
+            "capcount": "The number of captures. Currently deprecated - the capture count sent may or may not be the true number of captures. However, it will always be a positive non-zero number if the video is archived.",
+            "lastupdated": "The timestamp the data was retrieved from the server. Used internally to expire cache entries.",
+            "name": "The name of the service. Used in the UI.",
+            "note": "A footnote about the service. This could be different depending on conditions. For example, the Internet Archive has an extra passage if the item is dark.",
+            "rawraw": "The data used to check whether the video is archived on that particular service. For example, for GhostArchive, it would be the HTTP status code.",
+            "suppl": "Supplemental error message. Not used and currently inconsistent with what it returns and when.",
+            "metaonly": "True if only the metadata is archived. This value should not be relied on!",
+            "comments": "True if the comments are archived. This value should not be relied on!",
+        },
+    )
+
 
 @app.route("/nojs")
 async def formsubmit():
     vid = request.args.get("vid") or abort(400)
     return render_template("fid.html", data=(await find(vid)), vid=vid, nonoscript=True)
+
 
 @app.route("/ui/fid")
 async def fid():
--- findmedia.py
+++ findmedia.py
@@ -1,26 +1,45 @@
-import requests, config, urllib.parse
 import time
+import urllib.parse
 
+import requests
 from requests.auth import HTTPBasicAuth
 
+import config
+
 GCACHE = {}
-GNOTE  = ""
-GNAME  = "GhostArchive"
+GNOTE = ""
+GNAME = "GhostArchive"
+
+
 def ghostget(vid):
     if GCACHE.get(vid) and time.time() - GCACHE[vid]["lastupdated"] < 10:
         return GCACHE[vid]
     lien = f"https://ghostarchive.org/varchive/{vid}"
     response = requests.get(lien)
     archived = response.status_code == 200
-    ret = {"capcount": 1 if archived else 0, "archived": archived, "rawraw": response.status_code, "suppl": None, "lastupdated": time.time(), "note": GNOTE, "name": GNAME, "available": lien if archived else None, "metaonly": False, "comments": False}
+    ret = {
+        "capcount": 1 if archived else 0,
+        "archived": archived,
+        "rawraw": response.status_code,
+        "suppl": None,
+        "lastupdated": time.time(),
+        "note": GNOTE,
+        "name": GNAME,
+        "available": lien if archived else None,
+        "metaonly": False,
+        "comments": False,
+    }
     GCACHE[vid] = ret
     return ret
 
+
 FILNOTE = ""
 FILNAME = "Filmot"
-FILCACHE= {}
-FILDEL  = 2
-FILAST  = 0
+FILCACHE = {}
+FILDEL = 2
+FILAST = 0
+
+
 def filmot(vid):
     global FILAST
     if FILCACHE.get(vid) and time.time() - FILCACHE[vid]["lastupdated"] < 20:
@@ -32,13 +51,27 @@ def filmot(vid):
     FILAST = time.time()
     data = res.json()
     archived = bool(data)
-    ret = {"capcount": 1 if archived else 0, "archived": archived, "suppl": None, "rawraw": res.text, "lastupdated": time.time(), "note": FILNOTE, "name": FILNAME, "available": False, "metaonly": True, "comments": False}
+    ret = {
+        "capcount": 1 if archived else 0,
+        "archived": archived,
+        "suppl": None,
+        "rawraw": res.text,
+        "lastupdated": time.time(),
+        "note": FILNOTE,
+        "name": FILNAME,
+        "available": False,
+        "metaonly": True,
+        "comments": False,
+    }
     FILCACHE[vid] = ret
     return ret
 
+
 YACACHE = {}
-YANOTE  = "To retrieve a video from #youtubearchive, join #youtubearchive on hackint IRC and ask for help. Remember <a href='https://wiki.archiveteam.org/index.php/Archiveteam:IRC#How_do_I_chat_on_IRC?'>IRC etiquette</a>!"
-YANAME  = "#youtubearchive"
+YANOTE = "To retrieve a video from #youtubearchive, join #youtubearchive on hackint IRC and ask for help. Remember <a href='https://wiki.archiveteam.org/index.php/Archiveteam:IRC#How_do_I_chat_on_IRC?'>IRC etiquette</a>!"
+YANAME = "#youtubearchive"
+
+
 def yairc(vid):
     if not config.ya.enabled:
         return {}
@@ -47,7 +80,7 @@ def yairc(vid):
     auth = HTTPBasicAuth(config.ya.username, config.ya.password)
     data = requests.get("https://ya.borg.xyz/cgi-bin/capture-count?v=" + vid, auth=auth).text
     comments = False
-    commentcount = requests.get("https://ya.borg.xyz/cgi-bin/capture-comment-counts?v="+vid, auth=auth).text
+    commentcount = requests.get("https://ya.borg.xyz/cgi-bin/capture-comment-counts?v=" + vid, auth=auth).text
     counts = [i for i in commentcount.split("\n") if i.strip("∅\n") and i.strip() != "0"]
     if counts:
         comments = True
@@ -60,41 +93,77 @@ def yairc(vid):
         supplemental = "BAD_VID"
     archived = bool(count)
     NAHNOTE = YANOTE if archived else ""
-    ret = {"capcount": count, "archived": archived, "rawraw": (data, commentcount), "suppl": supplemental, "lastupdated": time.time(), "name": YANAME, "note": NAHNOTE, "metaonly": False, "comments": comments}
+    ret = {
+        "capcount": count,
+        "archived": archived,
+        "rawraw": (data, commentcount),
+        "suppl": supplemental,
+        "lastupdated": time.time(),
+        "name": YANAME,
+        "note": NAHNOTE,
+        "metaonly": False,
+        "comments": comments,
+    }
     YACACHE[vid] = ret
     return ret
 
+
 WBMCACHE = {}
-WBMNOTE  = ""
-WBMNAME  = "Wayback Machine"
+WBMNOTE = ""
+WBMNAME = "Wayback Machine"
+
+
 def wbm(vid):
     if WBMCACHE.get(vid) and time.time() - WBMCACHE[vid]["lastupdated"] < 6000:
         return WBMCACHE[vid]
-    response = requests.get(f"https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/{vid}", allow_redirects=False)
+    response = requests.get(
+        f"https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/{vid}", allow_redirects=False
+    )
     archived = True if response.headers.get("location") else False
     lien = response.headers.get("location")
     ismeta = False
     response2 = None
     if not archived:
-        check = urllib.parse.quote(f"https://youtube.com/watch?v={vid}", safe="") # not exhaustive but...
+        check = urllib.parse.quote(f"https://youtube.com/watch?v={vid}", safe="")  # not exhaustive but...
         response2 = requests.get(f"https://archive.org/wayback/available?url={check}").json()
         if response2["archived_snapshots"]:
             archived = True
             ismeta = True
             lien = response2["archived_snapshots"]["closest"]["url"]
-    ret = {"capcount": 1 if archived else 0, "archived": archived, "rawraw": (response.headers.get("location"), response2), "suppl": "NOIMPL", "available": lien, "lastupdated": time.time(), "name": WBMNAME, "note": WBMNOTE, "metaonly": ismeta, "comments": False}
+    ret = {
+        "capcount": 1 if archived else 0,
+        "archived": archived,
+        "rawraw": (response.headers.get("location"), response2),
+        "suppl": "NOIMPL",
+        "available": lien,
+        "lastupdated": time.time(),
+        "name": WBMNAME,
+        "note": WBMNOTE,
+        "metaonly": ismeta,
+        "comments": False,
+    }
     WBMCACHE[vid] = ret
     return ret
 
+
 IACACHE = {}
-IANOTE  = "Even if it isn't found here, it might still be in the Internet Archive. This site only checks for 'youtube-VIDEOID items."
-IANAME  = "Internet Archive"
+IANOTE = "Even if it isn't found here, it might still be in the Internet Archive. This site only checks for 'youtube-VIDEOID items."
+IANAME = "Internet Archive"
+
+
 def iai(vid):
     if IACACHE.get(vid) and time.time() - IACACHE[vid]["lastupdated"] < 60:
         return IACACHE[vid]
     data = requests.get("https://archive.org/metadata/youtube-" + vid).json()
     if not data:
-        ret = {"capcount": 0, "archived": False, "rawraw": data, "lastupdated": time.time(), "name": IANAME, "note": IANOTE}
+        ret = {
+            "capcount": 0,
+            "archived": False,
+            "rawraw": data,
+            "lastupdated": time.time(),
+            "name": IANAME,
+            "note": IANOTE,
+        }
         IACACHE[vid] = ret
         return ret
     capcount = 1
@@ -102,7 +171,17 @@ def iai(vid):
     lien = f"https://archive.org/details/youtube-{vid}"
     if data.get("is_dark"):
         capcount = 0
-        IANOT = "This item is currently unavailable to the general public.<br>"  + IANOTE
-    ret = {"capcount": capcount, "archived": True, "rawraw": data, "lastupdated": time.time(), "name": IANAME, "note": IANOT, "available": lien, "metaonly": False, "comments": False}
+        IANOT = "This item is currently unavailable to the general public.<br>" + IANOTE
+    ret = {
+        "capcount": capcount,
+        "archived": True,
+        "rawraw": data,
+        "lastupdated": time.time(),
+        "name": IANAME,
+        "note": IANOT,
+        "available": lien,
+        "metaonly": False,
+        "comments": False,
+    }
     IACACHE[vid] = ret
     return ret
