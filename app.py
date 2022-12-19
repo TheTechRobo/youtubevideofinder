805fcb2089a533e56511cb3da5a6c2769367e9d6
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-12-19 11:47:38 -0500

    improve error messages and make things a tad more modular

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 import re
 import lostmediafinder
 
-from flask import *
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 @app.route("/find/<id>")
-async def find(id):
+async def youtube(id):
     if not re.match(r"^[A-Za-z0-9_-]{10}[AEIMQUYcgkosw048]$", id):
         return {"status": "bad.id", "true": True, "id": None}, 400
-    return (await lostmediafinder.Response.generateAsync(id)).json()
+    return (await lostmediafinder.YouTubeResponse.generateAsync(id)).json()
+
+@app.route("/api/v<int:v>/<site>/<id>")
+@app.route("/api/v<int:v>/<id>")
+async def findapi(v, id, site="query"):
+    if v == 1:
+        return "This API version is no longer supported.", 410
+    if v != 2:
+        return "Unrecognised API version", 404
+    if site == "query":
+        site = request.args.get("site") or abort(400)
+    if site == "youtube":
+        return await youtube(id)
+    return "Unrecognised site", 404
 
 @app.route("/")
-async def ui():
+async def index():
     return render_template("init.html")
 
 @app.route("/api")
 async def api():
-    responseDocstring = lostmediafinder.Response.__doc__
+    responseDocstring = lostmediafinder.YouTubeResponse.__doc__
     serviceDocstring = lostmediafinder.Service.__doc__
     # TODO: Parse that
     # This works fine for now tho
     return render_template("api.html", fields=responseDocstring, services=serviceDocstring, type=type)
-
-@app.route("/nojs")
-async def formsubmit():
-    vid = request.args.get("vid") or abort(400)
-    return render_template("fid.html", data=(await find(vid)), vid=vid, nonoscript=True)
-
-@app.route("/ui/fid")
-async def fid():
-    vid = request.args.get("vid") or abort(400)
-    return render_template("fid.html", data=find(vid), vid=vid)
